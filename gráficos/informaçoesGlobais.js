40c2ebe450544fa39f0d4dbaf2dc9c1f7ee7cd52
PPAAGGOOTTTTOO <127508503+PPAAGGOOTTTTOO@users.noreply.github.com>
2024-10-04 08:59:06 -0300

    teste

--- gráficos/informaçoesGlobais.js
+++ gráficos/informaçoesGlobais.js
@@ -4,7 +4,7 @@ async function visualizarInfomacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.classList.add('Graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
